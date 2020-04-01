bb9e21807ad73765825b1dd160e7ff3ef31f4910
Gavin Starr <gistarr@pointpark.edu>
2020-04-01 16:22:33 -0400

    Added Buttons to Faq Page

---
+++ FamilyHouse/FamilyHouse/ViewControllerFaqForFam.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerFaqForFam.swift
+//  FamilyHouse
+//
+//  Created by Gavin Starr on 3/25/20.
+//  Copyright © 2020 PPU. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerFaqForFam: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
