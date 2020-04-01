22f90612113c28d4256b2ba6c06557a23f7c89fa
Emily VanderMey <40799536+emmyvander@users.noreply.github.com>
2020-04-01 17:35:22 -0400

    04.01.20 - added events to full project

---
+++ FamilyHouse/FamilyHouse/ViewControllerFunctions.swift
@@ -0,0 +1,35 @@
+//
+//  ViewControllerFunctions.swift
+//  test3
+//
+//  Created by Jeffrey Seaman on 3/17/20.
+//  Copyright © 2020 PPU. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerFunctions: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setupContactButton()
+    }
+    
+  
+    func setupContactButton(){
+         let menuBtn = UIButton(type: .custom)
+         menuBtn.frame = CGRect(x: 0.0, y: 0.0, width: 20, height: 20)
+         menuBtn.setImage(UIImage(named:"contactalert"), for: .normal)
+         //menuBtn.addTarget(self, action: #selector(ViewControllerLinens.onMenuButtonPressed(_:)), for: UIControl.Event.touchUpInside)
+         
+         let menuBarItem = UIBarButtonItem(customView: menuBtn)
+         let currWidth = menuBarItem.customView?.widthAnchor.constraint(equalToConstant: 24)
+         currWidth?.isActive = true
+         let currHeight = menuBarItem.customView?.heightAnchor.constraint(equalToConstant: 24)
+         currHeight?.isActive = true
+         self.navigationItem.rightBarButtonItem = menuBarItem
+    }
+
+}
